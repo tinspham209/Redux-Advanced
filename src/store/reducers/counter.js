e2751ac7819e890d3d2db6dececb2d67df3f5dd3
tinspham <tindeptrai1@gmail.com>
2019-12-27 01:38:36 +0700

    Using utility fuctions

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,33 +1,22 @@
-import * as actionTypes from '../actions/actionType';
-
+import * as actionTypes from "../actions/actionType";
+import { updatedObject } from "../utility";
 const initialState = {
-    counter: 0
+  counter: 0
 };
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1;
-            return newState;
-        case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
-        case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case actionTypes.INCREMENT:
+      return updatedObject(state, { counter: state.counter + 1 });
+    case actionTypes.DECREMENT:
+      return updatedObject(state, { counter: state.counter - 1 });
+    case actionTypes.ADD:
+      return updatedObject(state, { counter: state.counter + action.val });
+    case actionTypes.SUBTRACT:
+      return updatedObject(state, { counter: state.counter - action.val });
+    default:
+      return state;
+  }
 };
 
 export default reducer;
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,28 +1,26 @@
-import * as actionTypes from '../actions/actionType';
-
+import * as actionTypes from "../actions/actionType";
+import { updatedObject } from "../utility";
 const initialState = {
-    results: []
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: action.result })
-            }
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return updatedObject(state, {
+        results: state.results.concat({ id: new Date(), value: action.result })
+      });
+    case actionTypes.DELETE_RESULT:
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1)
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElId
+      );
+      return updatedObject(state, { results: updatedArray });
+    default:
+      return state;
+  }
 };
 
 export default reducer;
